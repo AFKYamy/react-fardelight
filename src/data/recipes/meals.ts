a1a4aae69c9de3f8e2002426a94ec7d36b2924f1
Asus TUF <honzahoangminhquan1234@gmail.com>
2026-02-11 23:16:58 +0100

    refactor: fixed a wrong file name and reduced amount of lines where possible

--- src/data/recipes/meals.ts
+++ src/data/recipes/meals.ts
@@ -8,40 +8,19 @@ export const meals: MealType[] = [
         type: "meals",
         description: "Fruit Salad can be crafted with a Bowl, two Berries (either Glow Berries or Sweet Berries), two Melon Slices, one Pumpkin Slice, and an Apple. Once eaten, Fruit Salad provides the Regeneration effect for 5 seconds.",
         ingredients: [
-            { 
-                name: "Bowl",
-                amount: 1
-            },
-            { 
-                name: "Berries",
-                amount: 2
-            },
-            { 
-                name: "Melon Slices",
-                amount: 2
-            },
-            { 
-                name: "Pumpkin Slice",
-                amount: 1
-            },
-            { 
-                name: "Apple",
-                amount: 1
-            }
+            { name: "Bowl", amount: 1 },
+            { name: "Berries", amount: 2 },
+            { name: "Melon Slices", amount: 2 },
+            { name: "Pumpkin Slice", amount: 1 },
+            { name: "Apple", amount: 1 }
         ],
         crafting: {
             source: "Crafting Table",
-            img: "/assets/food/Fruid_Salad_Crafting.png"
+            img: "/assets/food/Fruit_Salad_Crafting.png"
         },
-        stats: {
-            hunger: 6,
-            saturation: 3.6
-        },
+        stats: { hunger: 6, saturation: 3.6 },
         effects: [
-            {
-                name: "Regeneration",
-                duration: 5000
-            }
+            { name: "Regeneration", duration: 5000 }
         ],
         img: "/assets/food/Fruit_Salad.png",
     },
@@ -52,32 +31,17 @@ export const meals: MealType[] = [
         type: "meals",
         description: "Nether Salad can be crafted with a Bowl, Crimson Fungus, and Warped Fungus.",
         ingredients: [
-            {
-                name: "Bowl",
-                amount: 1
-            },
-            {
-                name: "Crimson Fungus",
-                amount: 1
-            },
-            {
-                name: "Warped Fungus",
-                amount: 1
-            }
+            { name: "Bowl", amount: 1 },
+            { name: "Crimson Fungus", amount: 1 },
+            { name: "Warped Fungus", amount: 1 }
         ],
         crafting: {
             source: "Crafting Table",
             img: "/assets/food/Nether_Salad_Crafting.png"
         },
-        stats: {
-            hunger: 5,
-            saturation: 4
-        },
+        stats: { hunger: 5, saturation: 4 },
         effects: [
-            {
-                name: "Nausea",
-                duration: 12000
-            }
+            { name: "Nausea", duration: 12000 }
         ],
         img: "/assets/food/Nether_Salad.png",
     },
@@ -88,24 +52,15 @@ export const meals: MealType[] = [
         type: "meals",
         description: "It is obtained by cooking rice in a cooking pot (plated with a bowl) taking 100t (5s) and awarding 0.35xp",
         ingredients: [
-            {
-                name: "Rice",
-                amount: 1
-            }
+            { name: "Rice", amount: 1 }
         ],
         crafting: {
             source: "Cooking Pot",
             img: "/assets/food/Crafting_Cooked_Rice.png"
         },
-        stats: {
-            hunger: 6,
-            saturation: 5.8
-        },
+        stats: { hunger: 6, saturation: 5.8 },
         effects: [
-            {
-                name: "Comfort",
-                duration: 30000
-            }
+            { name: "Comfort", duration: 30000 }
         ],
         img: "/assets/food/Cooked_Rice.png",
     },
@@ -116,28 +71,16 @@ export const meals: MealType[] = [
         type: "meals",
         description: "In a Cooking Pot, add a Bone and either a Brown Mushroom, Red Mushroom or Glow Lichen.",
         ingredients: [
-            {
-                name: "Bone",
-                amount: 1
-            },
-            {
-                name: "Brown Mushroom",
-                amount: 1
-            }
+            { name: "Bone", amount: 1 },
+            { name: "Brown Mushroom", amount: 1 }
         ],
         crafting: {
             source: "Cooking Pot",
             img: "/assets/food/Cooking_Bone_Broth.png"
         },
-        stats: {
-            hunger: 8,
-            saturation: 11.2
-        },
+        stats: { hunger: 8, saturation: 11.2 },
         effects: [
-            {
-                name: "Comfort",
-                duration: 60000
-            }
+            { name: "Comfort", duration: 60000 }
         ],
         img: "/assets/food/Bone_Broth.png",
     },
