68cffb589026795591d082a87213bf1a5b5503c9
Asus TUF <honzahoangminhquan1234@gmail.com>
2026-02-11 23:12:14 +0100

    refactor: changed up types to semicolons and reserve commas only for nested objects

--- src/pages/recipes/cards/Card.tsx
+++ src/pages/recipes/cards/Card.tsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 // types
 type Card = {
-    slug: string,
-    name: string,
+    slug: string;
+    name: string;
     img: string
 }
 
--- src/types/recipes/foods.ts
+++ src/types/recipes/foods.ts
@@ -1,13 +1,13 @@
 export type MealType = {
-    id: string,
-    slug: string,
-    name: string,
-    type: string,
-    description: string,
+    id: string;
+    slug: string;
+    name: string;
+    type: string;
+    description: string;
     ingredients: {
         name: string,
         amount: number
-    }[],
+    }[];
     crafting: {
         source: string,
         img: string
@@ -15,10 +15,10 @@ export type MealType = {
     stats: {
         hunger: number,
         saturation: number
-    },
+    };
     effects: {
         name: string,
         duration: number
-    }[],
+    }[];
     img: string
 }
