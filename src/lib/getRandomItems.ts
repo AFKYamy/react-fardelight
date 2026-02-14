004ec45cd1b2c936d277b592d406a56cc45432a0
Asus TUF <honzahoangminhquan1234@gmail.com>
2026-02-14 19:55:13 +0100

    feat: randomized order of recipes

--- src/lib/getRandomItems.ts
+++ src/lib/getRandomItems.ts
@@ -1,5 +1,9 @@
 export function getRandomItems<T>(items: T[], count: number) {
-    const shuffled = [...items].sort(() => 0.5 - Math.random());
+    const shuffled = shuffleItems(items)
 
     return shuffled.slice(0, count);
+}
+
+export function shuffleItems<T>(items: T[]) {
+    return [...items].sort(() => 0.5 - Math.random());
 }
--- src/pages/recipes/cards/Cards.tsx
+++ src/pages/recipes/cards/Cards.tsx
@@ -4,13 +4,17 @@ import { useRecipesContext } from "@/contexts/RecipesContext";
 // components
 import Card from "./Card";
 
+// lib
+import { shuffleItems } from "@/lib/getRandomItems";
+
 export default function Cards() {
     const { selectedRecipes } = useRecipesContext();
+    const shuffledRecipes = shuffleItems(selectedRecipes);
 
     return (
         <section className="flex justify-between gap-7 flex-wrap mx-auto my-20 container">
             {
-                selectedRecipes.map((rec) => (
+                shuffledRecipes.map((rec) => (
                     <Card
                         key={rec.id}
                         rec={rec}
