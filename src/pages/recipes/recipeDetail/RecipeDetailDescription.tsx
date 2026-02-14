be56f14256426a5b41d7a8b9b4ec53abaf181712
Asus TUF <honzahoangminhquan1234@gmail.com>
2026-02-14 16:51:56 +0100

    feat: added link back to recipes from recipe detail page

--- src/pages/recipes/recipeDetail/RecipeDetailDescription.tsx
+++ src/pages/recipes/recipeDetail/RecipeDetailDescription.tsx
@@ -1,6 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function RecipeDetailDescription({ desc }: { desc: string }) {
     return (
-        <section className="my-15">
+        <section className="flex flex-col gap-5 my-15">
+            <Link 
+                to="/recipes"
+                className="font-bold italic hover:text-gray"
+            >
+                Go back to Recipes
+            </Link>
             <p>{desc}</p>
         </section>
     )
